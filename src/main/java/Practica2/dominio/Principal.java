e1c6521e30fb3685bbc5c51e0c2fc1eab00336d6
ana <andelamo@myuax.com>
2023-03-23 19:44:56 +0100

    Clase main

--- src/main/java/Practica2/dominio/Principal.java
+++ src/main/java/Practica2/dominio/Principal.java
@@ -1,7 +1,34 @@
 package Practica2.dominio;
 
+import java.util.concurrent.TimeUnit;
+import java.lang.InterruptedException;
 public class Principal {
     public static void main(String[] args) {
+        try{
+            Tablero tablero = new Tablero();
+            System.out.println("SIMULACIÓN CON TABLERO LEÍDO");
+
+            tablero.leerEstadoActual();
+            System.out.println(tablero);
+            for(int i = 0; i <=5; i++){
+                TimeUnit.SECONDS.sleep(1);
+                tablero.transitarAlEstadoSiguiente();
+                System.out.println(tablero);
+
+            }
+
+            System.out.println("SIMULACIÓN CON TABLERO GENERADO POR MONTECARLO");
+            tablero.generarEstadoActualPorMontecarlo();
+            System.out.println(tablero);
+            for(int i = 0; i <=15; i++){
+                TimeUnit.SECONDS.sleep(1);
+                tablero.transitarAlEstadoSiguiente();
+                System.out.println(tablero);
+
+            }
+        }catch(InterruptedException e){
+            System.out.println("Error en la simulación");
+        }
 
     }
 }
