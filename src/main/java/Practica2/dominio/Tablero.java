e653fed629c6ea6edeb00c162276d2a6b11c3ce0
ana <andelamo@myuax.com>
2023-03-23 19:39:25 +0100

    Metodos vacios clase Tablero

--- src/main/java/Practica2/dominio/Tablero.java
+++ src/main/java/Practica2/dominio/Tablero.java
@@ -1,4 +1,26 @@
 package Practica2.dominio;
 
+import java.util.Arrays;
+
 public class Tablero {
+    private static int DIMENSION = 30;
+    private int[][] estadoActual;
+    private int[][] estadoSiguiente = new int[DIMENSION][DIMENSION];
+
+    public void leerEstadoActual(){
+
+    }
+
+    public void generarEstadoActualPorMontecarlo(){
+
+    }
+
+    public void transitarAlEstadoSiguiente(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
