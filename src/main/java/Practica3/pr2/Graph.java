8e11fb785b816ccfd09cfc570bbd304915a81004
ana <andelamo@myuax.com>
2023-03-23 20:08:20 +0100

    metodos addVertex y addEdge

--- src/main/java/Practica3/pr2/Graph.java
+++ src/main/java/Practica3/pr2/Graph.java
@@ -6,4 +6,22 @@ import java.util.Set;
 
 public class Graph<V>{
     private Map<V, Set<V>> adjacencyList = new HashMap<>();
+
+    public boolean addVertex(V v){
+        if(adjacencyList.containsKey(v)){
+            return false;
+        }
+        adjacencyList.put(v, null);
+        return true;
+
+    }
+
+    public boolean addEdge(V v1, V v2){
+        if(!adjacencyList.containsKey(v1) || !adjacencyList.containsKey(v2)){
+            return false;
+        }
+        adjacencyList.get(v1).add(v2);
+        adjacencyList.get(v2).add(v1);
+        return true;
+    }
 }
