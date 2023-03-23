10088784e9df9a9626a51f53b98b4e92d5c6de91
ana <andelamo@myuax.com>
2023-03-23 19:28:41 +0100

    metodo montecarlo con main

--- src/main/java/aplicacion/Principal.java
+++ src/main/java/aplicacion/Principal.java
@@ -2,7 +2,8 @@ package aplicacion;
 import mates.Matematicas;
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("El número PI es: " + Matematicas.generarNumPIMontecarlo(Integer.parseInt(args[0])));
+        Matematicas matematicas = new Matematicas();
+        System.out.println("El número PI es: " + Matematicas.generarNumPIMontecarlo(10));
 
     }
 }
--- src/main/java/mates/Matematicas.java
+++ src/main/java/mates/Matematicas.java
@@ -3,19 +3,17 @@ import java.util.Scanner;
 public class Matematicas {
     public static double generarNumPIMontecarlo(long pasos){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese la cantidad de puntos que quiere generar: ");
-        int puntosTotales = sc.nextInt();
 
         int aciertos = 0;
         double areaCuadrado = 4;
-        for(int i = 0; i< puntosTotales; i++){
+        for(int i = 0; i< pasos; i++){
             double x = Math.random();
             double y = Math.random();
             if(Math.pow(x, 2) + Math.pow(y, 2) <= 1){
                 aciertos++;
             }
         }
-        double pi = (aciertos * areaCuadrado) / puntosTotales;
+        double pi = (aciertos * areaCuadrado) / pasos;
         return pi;
     }
 }
