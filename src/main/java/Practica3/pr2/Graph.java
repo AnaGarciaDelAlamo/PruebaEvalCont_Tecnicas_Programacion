8ac99e49eb40ecd57dc50125db773ffd0a5e4f3b
ana <andelamo@myuax.com>
2023-03-23 20:04:26 +0100

    practica3. Busqueda de un grafo

---
+++ src/main/java/Practica3/pr2/Graph.java
@@ -0,0 +1,9 @@
+package Practica3.pr2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Graph<V>{
+    private Map<V, Set<V>> adjacencyList = new HashMap<>();
+}
--- src/main/java/aplicacion/Principal.java
+++ src/main/java/aplicacion/Principal.java
@@ -2,7 +2,6 @@ package aplicacion;
 import mates.Matematicas;
 public class Principal {
     public static void main(String[] args) {
-        Matematicas matematicas = new Matematicas();
         System.out.println("El número PI es: " + Matematicas.generarNumPIMontecarlo(10));
 
     }
